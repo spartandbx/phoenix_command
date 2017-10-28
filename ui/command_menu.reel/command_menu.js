4e031a11ee993e99f4d3bf52b77523a474093c23
spartandbx <noreply>
2017-10-28 02:51:11 +0000

    Add component command_menu

---
+++ ui/command_menu.reel/command_menu.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/command_menu.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Command_menu
+ * @extends Component
+ */
+exports.Command_menu = Component.specialize(/** @lends Command_menu# */ {
+    constructor: {
+        value: function Command_menu() {
+            this.super();
+        }
+    }
+});
